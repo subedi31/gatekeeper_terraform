b0c7542dca64dc7c07de2c0b060c86e9db889d8a
Binod Adhikari <074msice006.binod@pcampus.edu.np>
2023-06-21 12:51:23 +0545

    change on action: add uuses

--- k8s-gatekeeper/main.tf
+++ k8s-gatekeeper/main.tf
@@ -3,12 +3,12 @@ provider "aws" {
   region  = "us-west-1"
 }
 provider "kubernetes" {
-  config_path = "~/.kube/config"
+  config_path = "${path.root}/.kube/config"
 }
 
 provider "helm" {
   kubernetes {
-  config_path = "~/.kube/config"
+  config_path = "${path.root}/.kube/config"
   }
 }
 
