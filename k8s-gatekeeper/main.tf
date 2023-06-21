dfe096fd97646966d3cb94468867e31e3e42875d
Binod Adhikari <074msice006.binod@pcampus.edu.np>
2023-06-21 15:05:59 +0545

    change on action: add uuses

--- k8s-gatekeeper/main.tf
+++ k8s-gatekeeper/main.tf
@@ -23,7 +23,6 @@ resource "helm_release" "gatekeeper" {
   repository = "https://open-policy-agent.github.io/gatekeeper/charts"
   name       = var.helm_release_name
   namespace  = var.namespace
-  version    = "3.5.1"
 
   depends_on = [
     kubernetes_namespace.gatekeeper
